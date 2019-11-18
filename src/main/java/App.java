90631fcc5ef0825cb81259437b8fb4ad1202812f
denningkevin <denningkev@gmail.com>
2019-11-18 10:59:29 +0300

    add dependancies to buildGradle

---
+++ src/main/java/App.java
@@ -0,0 +1,13 @@
+import models.Post;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
